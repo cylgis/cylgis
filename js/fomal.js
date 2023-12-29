8e218dc48c16c0a6b72eb415ed15c76801424c36
cylgis <1779706326@qq.com>
2023-12-29 10:58:51 +0800

    successover1

--- js/fomal.js
+++ js/fomal.js
@@ -1123,13 +1123,14 @@ function createtime1() {
     `欢迎来到cyl&cxqの小家!`,
     `Future is now`,
     `
-        
-███████    ██   ██     ███        
-██          ██ ██      ███        
-██           ██        ███      
-██           ██        ███      
-███████      ██        ████████    
-                                              
+                 _           _       
+                | |         (_)      
+   ___   _   _  | |   __ _   _   ___ 
+  / __| | | | | | |  / _` | | | / __|
+ | (__  | |_| | | | | (_| | | | \__ \
+  \___|  \__, | |_|  \__, | |_| |___/
+          __/ |       __/ |          
+         |___/       |___/           
 `,
     "小站已经苟活",
     dnum,
@@ -2792,11 +2793,11 @@ var now = new Date();
 function createtime() {
   // 当前时间
   now.setTime(now.getTime() + 1000);
-  var start = new Date("08/01/2022 00:00:00"); // 旅行者1号开始计算的时间
+  var start = new Date("08/01/2023 00:00:00"); // 旅行者1号开始计算的时间
   var dis = Math.trunc(23400000000 + ((now - start) / 1000) * 17); // 距离=秒数*速度 记住转换毫秒
   var unit = (dis / 149600000).toFixed(6);  // 天文单位
   // 网站诞生时间
-  var grt = new Date("08/09/2022 00:00:00");
+  var grt = new Date("20/08/2023 00:00:00");
   var days = (now - grt) / 1e3 / 60 / 60 / 24,
     dnum = Math.floor(days),
     hours = (now - grt) / 1e3 / 60 / 60 - 24 * dnum,
@@ -2811,8 +2812,8 @@ function createtime() {
   let currentTimeHtml = "";
   (currentTimeHtml =
     hnum < 18 && hnum >= 9
-      ? `<img class='boardsign' src='https://lskypro.acozycotage.net/Fomalhaut/badge/F小屋-科研摸鱼中.svg' title='什么时候能够实现财富自由呀~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${dis} 千米，约为 ${unit} 个天文单位 🚀</div>`
-      : `<img class='boardsign' src='https://lskypro.acozycotage.net/Fomalhaut/badge/F小屋-下班休息啦.svg' title='下班了就该开开心心地玩耍~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${dis} 千米，约为 ${unit} 个天文单位 🚀</div>`),
+      ? `<img class='boardsign' src='https://picgo.cylgis.top/picture/asses/2023/12/29/开心.svg' title='两年后的千万富翁就是你~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i>`
+      : `<img class='boardsign' src='https://picgo.cylgis.top/picture/asses/2023/12/29/开心.svg' title='下班了随便逛逛吧~'><br>`),
     document.getElementById("workboard") &&
     (document.getElementById("workboard").innerHTML = currentTimeHtml);
 }
