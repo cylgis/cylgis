6d3fcdd7b40b344b7fb9eba9e93d1c082315d573
cylgis <1779706326@qq.com>
2023-12-29 11:36:07 +0800

    successover1

--- js/fomal.js
+++ js/fomal.js
@@ -290,9 +290,9 @@ if(ipLoacation != null){
     // console.log("Pjax无法获取#welcome-info元素🙄🙄🙄")
   }
 }
-window.onload = showWelcome;
+//window.onload = showWelcome;
 // 如果使用了pjax在加上下面这行代码
-document.addEventListener('pjax:complete', showWelcome);
+//document.addEventListener('pjax:complete', showWelcome);
 
 /* 欢迎信息 end */
 
