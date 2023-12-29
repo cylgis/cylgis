66af6e2496ad72e8f4384a2e32541f64a318f228
cylgis <1779706326@qq.com>
2023-12-29 16:27:14 +0800

    successover1

--- js/fomal.js
+++ js/fomal.js
@@ -2917,7 +2917,7 @@ function clearItem() {
 
 // 设置字体
 if (localStorage.getItem("font") == undefined) {
-  localStorage.setItem("font", "LXGW");
+  localStorage.setItem("font", "HYTMR");
 }
 setFont(localStorage.getItem("font"));
 function setFont(n) {
